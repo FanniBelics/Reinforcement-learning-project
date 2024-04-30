1599313b039eae0323ec9e3d9ca7dfa1ee628f90
FanniBelics <fanni.belics@gmail.com>
2024-04-30 20:35:02 +0200

    Preparing enviroment, heuristics and actions on observations

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using static BlueyController;
+
+public class BalloonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obsticle")
+        {
+            Debug.Log(collision.gameObject.name + " Collided!");
+        }
+        if(collision.collider.tag == "Floor")
+        {
+            Debug.Log("Floor collided");
+            FindObjectOfType<BlueyController>().EndThisEpisode();
+        }
+    }
+}
--- Assets/Scripts/BlueyController.cs
+++ Assets/Scripts/BlueyController.cs
@@ -1,39 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BlueyController : Agent
 {
     public float speed = 5;
-    private GameObject balloon;
-    public Transform targetTransform;
+    public GameObject balloonPrefab;
+    private Vector3 startingPosition = new Vector3(0.0f, 1.5f, -8.0f);
+    public Rigidbody rb;
+    private GameObject newBalloon;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("Start...");
     }
 
     public override void OnEpisodeBegin()
     {
+        Debug.Log("Episode begins");
         float xPosition = UnityEngine.Random.RandomRange(-3, 11);
         float zPosition = UnityEngine.Random.RandomRange(-7, 10);
 
-        targetTransform.localPosition = new Vector3(xPosition, 10, zPosition);
+        newBalloon = Instantiate(balloonPrefab, new Vector3(xPosition, 10, zPosition), Quaternion.identity);
+        transform.localPosition = startingPosition;
     }
 
     // Update is called once per frame
-    void Update()
+     /*void Update()
+     {
+         if (Input.GetKey("w"))
+        {
+             transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime * actionDirection)
+         }
+         if (Input.GetKey("d"))
+             transform.Rotate(0.0f, +0.5f, 0.0f);
+
+         if (Input.GetKey("a"))
+             transform.Rotate(0.0f, -0.5f, 0.0f);
+
+         if (Input.GetKey("s"))
+         {
+             transform.Translate(Vector3.back * speed * Time.fixedDeltaTime);
+         }
+     }*/
+
+    public override void CollectObservations(VectorSensor sensor)
     {
+        //Not needed because of the ray sensors :D
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        var actionTaken = actions.ContinuousActions;
+
+        float actionDirection = actionTaken[0];
+        float actionSteering = actionTaken[1];
+
+        transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime * actionDirection);
+        transform.rotation = Quaternion.Euler(new Vector3(0, actionSteering * 180, 0));
+
+        AddReward((actionDirection + actionSteering) * 0.01f);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> actions = actionsOut.ContinuousActions;
+
+        actions[0] = 0;
+        actions[1] = 0;
+
         if (Input.GetKey("w"))
         {
-            transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+            actions[0] = 1;
+        }
+        if (Input.GetKey("s"))
+        {
+            actions[0] = -1;
         }
         if (Input.GetKey("d"))
-            transform.Rotate(0.0f, +0.5f, 0.0f);
+            actions[1] = +0.5f;
 
         if (Input.GetKey("a"))
-            transform.Rotate(0.0f, -0.5f, 0.0f);
+            actions[1] = -0.5f;
     }
+  
+
+    public void EndThisEpisode()
+    {
+        AddReward(-10);
+        Destroy(newBalloon);
+        EndEpisode();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision is with a furniture object
+        if (collision.gameObject.CompareTag("Obsticle") ||collision.gameObject.CompareTag("Walls"))
+        {
+            // Stop the agent's movement
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        if (collision.gameObject.CompareTag("Balloon")){
+            AddReward(1);
+        }
+    }
+
 }
